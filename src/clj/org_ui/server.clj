0e1e82c4c628297e4fcf285e3e7e53e9f72ce10c
Maria Neise <mneise@liveops.com>
2015-03-04 15:07:38 +1300

    Refactoring.

--- src/clj/org_ui/server.clj
+++ src/clj/org_ui/server.clj
@@ -4,7 +4,6 @@
             [org-ui.dev :refer [is-dev? inject-devmode-html start-figwheel]]
             [compojure.core :refer [GET defroutes]]
             [compojure.route :refer [resources]]
-            [compojure.handler :refer [api]]
             [net.cgrand.enlive-html :refer [deftemplate]]
             [ring.middleware.reload :as reload]
             [environ.core :refer [env]]
@@ -20,18 +19,17 @@
 
 (def http-handler
   (if is-dev?
-    (reload/wrap-reload (api #'routes))
-    (api routes)))
+    (reload/wrap-reload routes)
+    routes))
 
 (defn run [& [port]]
-  (defonce ^:private server
-    (do
-      (if is-dev? (start-figwheel))
-      (let [port (Integer. (or port (env :port) 10555))]
-        (print "Starting web server on port" port ".\n")
-        (run-jetty http-handler {:port port
-                          :join? false}))))
-  server)
+  (let [server (do
+                 (when is-dev? (start-figwheel))
+                 (let [port (Integer. (or port (env :port) 10555))]
+                   (print "Starting web server on port" port ".\n")
+                   (run-jetty http-handler {:port port
+                                            :join? false})))]
+    server))
 
 (defn -main [& [port]]
   (run port))
